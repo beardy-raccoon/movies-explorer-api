cb81077aa962c4d42b7f546bbdb6e8afcd04eb56
Vlad Meshcherinov <vladislav1310@gmail.com>
2022-10-08 14:00:38 +0400

    set not secure cookies

--- utils/getToken.js
+++ utils/getToken.js
@@ -7,9 +7,9 @@ const getToken = (res, user) => {
   const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
   res.cookie('jwt', token, {
     maxAge: 3600000 * 24 * 7,
-    httpOnly: true,
+    /* httpOnly: true,
     secure: false,
-    sameSite: true,
+    sameSite: true, */
   });
 };
 

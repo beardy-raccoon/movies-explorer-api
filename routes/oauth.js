3ca760613c1a74d69a582f76dbbe500ab10ecc86
Vlad Meshcherinov <vladislav1310@gmail.com>
2023-05-03 14:09:50 +0400

    fix routes

--- routes/oauth.js
+++ routes/oauth.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const { getAuthUrl, getAuthData } = require('../controllers/oauth');
 
 router.get('/google', getAuthUrl);
-router.get('google/redirect/:params', getAuthData);
+router.get('google/redirect', getAuthData);
 
 module.exports = router;

9f0d27b410d4b87074285dc01bbf72dac4f39971
Vlad Meshcherinov <vladislav1310@gmail.com>
2023-05-03 12:48:48 +0400

    run9

--- routes/public.js
+++ routes/public.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 const { signUp, signIn } = require('../controllers/users');
 const { signupValidation, signinValidation } = require('../middlewares/validation');
 
-router.post('/signup', signupValidation, signUp);
-router.post('/signin', signinValidation, signIn);
 router.get(
   '/random',
   (req, res) => res.send({ message: 'handling route /oauth' }),
 );
+router.post('/signup', signupValidation, signUp);
+router.post('/signin', signinValidation, signIn);
 
 module.exports = router;
